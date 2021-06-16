24ae8d4d0bb5925e1b262356f19caf4f3b99c64f
Hassaan Ahmad <59789600+hassaanahmed844@users.noreply.github.com>
2021-06-16 23:30:48 +0500

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HOMEPAGE extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int id = 234;
+    String name = "Hassaan";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("My name is $name and my ID is $id"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
